53ed4128c814cdc34b824f14e38c7b1a0ac456fd
Mike Muszynski <mmuszynski@gmail.com>
2020-09-06 20:12:54 -0400

    moved suit shapes into lazy properties

--- Sources/CardTable/PlayingCardView/SuitShapes.swift
+++ Sources/CardTable/PlayingCardView/SuitShapes.swift
@@ -11,22 +11,41 @@ import CardDeck
 import SVGParser
 
 extension PlayingCard.Suit {
-    var svgImage: SVGImageView {
-        var name = ""
-        switch self {
-        case .spade:
-            name = "Spade"
-        case .diamond:
-            name = "Diamond"
-        case .club:
-            name = "Club"
-        case .heart:
-            name = "Heart"
-        }
-        
-        let url = Bundle.module.url(forResource: name, withExtension: "svg")!
+    private static var spadeImage: SVGImageView {
+        let url = Bundle.module.url(forResource: "Spade", withExtension: "svg")!
         let image = try! SVGImage(contentsOf: url)
         return SVGImageView(image: image)
+    }
+    
+    private static var diamondImage: SVGImageView {
+        let url = Bundle.module.url(forResource: "Diamond", withExtension: "svg")!
+        let image = try! SVGImage(contentsOf: url)
+        return SVGImageView(image: image)
+    }
+    
+    private static var clubImage: SVGImageView {
+        let url = Bundle.module.url(forResource: "Club", withExtension: "svg")!
+        let image = try! SVGImage(contentsOf: url)
+        return SVGImageView(image: image)
+    }
+    
+    private static var heartImage: SVGImageView {
+        let url = Bundle.module.url(forResource: "Heart", withExtension: "svg")!
+        let image = try! SVGImage(contentsOf: url)
+        return SVGImageView(image: image)
+    }
+    
+    var svgImage: SVGImageView {
+        switch self {
+        case .spade:
+            return PlayingCard.Suit.spadeImage
+        case .diamond:
+            return PlayingCard.Suit.diamondImage
+        case .club:
+            return PlayingCard.Suit.clubImage
+        case .heart:
+            return PlayingCard.Suit.heartImage
+        }
     }
 }
 
@@ -37,8 +56,6 @@ struct SuitShapes_Previews: PreviewProvider {
             PlayingCard.Suit.club.svgImage
             PlayingCard.Suit.spade.svgImage
             PlayingCard.Suit.heart.svgImage
-            
-            SVGImageView(image: try! SVGImage(contentsOf: Bundle.module.url(forResource: "CLUB-11-JACK", withExtension: "svg")!))
         }
     }
 }
