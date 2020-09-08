30b1cc3141ea780ff8a61cb3c350bcf17a127f23
Mike Muszynski <mmuszynski@gmail.com>
2020-09-08 15:46:25 -0400

    fan working

--- Sources/CardTable/HandView.swift
+++ Sources/CardTable/HandView.swift
@@ -15,59 +15,95 @@ public struct HandView<CardType: Card>: View {
     @State var dragIndex: Int? = nil
     @GestureState var dragOffset: CGSize = .zero
     
+    @State var fuckYouXcode: String = "x"
+    
     public init(cards: Binding<Deck<CardType>>, selection: Binding<Set<Int>>) {
         self._cards = cards
         self._selection = selection
     }
     
-    var perCardOffsetAmount: Double {
-        let angle = Double(cards.count) * 2
-        return angle / Double(cards.count)
+    var perCardOffsetAmount: CGFloat {
+        return 40
     }
     
-    func fanAmount(_ index: Int) -> Double {
-        let mid = Double(self.cards.count) / 2 - 0.5
-        return (Double(index) - mid) * perCardOffsetAmount
+    var cardsOffset: CGFloat {
+        return self.dragOffset.width / perCardOffsetAmount
     }
     
-    func fanSpread(_ index: Int) -> CGSize {
-        let angle = CGFloat(fanAmount(index))
-        return CGSize(width: CGFloat(index - cards.count / 2) * CGFloat(cards.count), height: 0)
+    func proposedDropIndex(for offset: CGSize) -> Int? {
+        guard let current = dragIndex else { return nil }
+        let index = Int(offset.width / perCardOffsetAmount) + current
+        
+        fuckYouXcode = "\(current) \(index)"
+        
+        return index
     }
     
-    func index(from offset: CGFloat) -> Double {
-        let count = Double(cards.count)
-        return count * (Double(offset) + 0.5)
+    var cardOffsets: [CGFloat] {
+        let indices = self.cards.enumerated().map(\.offset)
+        let initialOffset = CGFloat(self.cards.count / 2) * perCardOffsetAmount
+        var output: [CGFloat]
+        
+        if let dragged = dragIndex, let proposedDrop = proposedDropIndex(for: dragOffset), dragged != proposedDrop {
+            output = indices.map { index in
+                
+                if proposedDrop < dragged {
+                    if (proposedDrop..<dragged).contains(index) {
+                        return CGFloat(index + 1) * perCardOffsetAmount
+                    }
+                } else {
+                    if (dragged+1...proposedDrop).contains(index) {
+                        return CGFloat(index - 1) * perCardOffsetAmount
+                    }
+                }
+                
+                return CGFloat(index) * perCardOffsetAmount
+            }
+        } else {
+            output = indices.map { CGFloat($0) * perCardOffsetAmount }
+        }
+        
+        return output.map { $0 - initialOffset }
     }
     
-    func reorderOffset(for offsetAmount: CGFloat) -> Int {
-        let i = index(from: offsetAmount)
-        return Int(i)
+    func angle(for offset: CGFloat) -> Angle {
+        return Angle(degrees: Double(offset * 0.05))
     }
     
     public var body: some View {
         ZStack {
             ForEach(0..<cards.count) { index in
                 CardView(card: cards[index])
-                    .zIndex(Double(dragIndex == index ? index + reorderOffset(for: dragOffset.width) : index))
+                    //composite to avoid the shadow on every element
                     .compositingGroup()
-                    .shadow(radius: 5)
-                    .offset(x: 0, y: selection.contains(index) ? -40 : 0)
-                    //.rotationEffect(Angle(degrees: fanAmount(index)), anchor: .bottom)
-                    .offset(fanSpread(index))
+                    //apply the shadow
+                    .shadow(radius: 2)
+                    //set the z index based on the order of the hand
+                    //plus, if the card is being dragged, update the index there as well
+                    .zIndex(Double(index) + Double(dragIndex == index ? cardsOffset : 0))
+                    //offset the card based on where it is in the hand index
+                    //offset the card further if it is the one being dragged
+                    .rotationEffect(angle(for: dragIndex == index ? dragOffset.width : 0))
+                    .offset(x: cardOffsets[index], y: 0)
                     .offset(dragIndex == index ? dragOffset : .zero)
-                    .shadow(color: selection.contains(index) ? .yellow : .clear, radius: 3, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
-                    .gesture(
-                        DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                            .updating($dragOffset, body: { (value, state, transaction) in
-                            dragIndex = index
-                            state = value.translation
-                            }).onEnded({ (gesture) in
-                                withAnimation {
-                                    dragIndex = nil
-                                }
-                            })
-                )
+                    .rotationEffect(angle(for: cardOffsets[index]), anchor: .bottom)
+                    //the actual drag gesture
+                    .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                                .updating($dragOffset, body: { (value, state, transaction) in
+                                    dragIndex = index
+                                    state = value.translation
+                                })
+                                .onEnded({ (gestureValue) in
+                                    let proposedDropIndex = self.proposedDropIndex(for: gestureValue.translation)!
+                                    var newIndex = proposedDropIndex
+                    
+                                    newIndex = newIndex < 0 ? 0 : newIndex
+                                    let thisCard = cards.remove(at: dragIndex!)
+                                    
+                                    newIndex = newIndex >= cards.count ? cards.endIndex : newIndex
+                                    cards.insert(thisCard, at: newIndex)
+                                }))
+                
             }
         }
     }
@@ -75,11 +111,10 @@ public struct HandView<CardType: Card>: View {
 
 struct HandView_Previews: PreviewProvider {
     static var previews: some View {
-        let deck = Deck(PlayingCard.fullDeck[0..<26]).sorted(by: PlayingCard.defaultAscendingOrder)
+        let deck = Deck(PlayingCard.fullDeck[0..<10]).sorted(by: PlayingCard.defaultAscendingOrder)
         
         BindingProvider(deck) { binding in
             HandView(cards: binding, selection: .constant(Set()))
-                .scaledToFit()
         }
     }
 }
